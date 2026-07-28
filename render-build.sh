9d554957ca40a95745dfc0438d3070a6195aaa15
Teddy <tthoronjo@gmail.com>
2026-07-28 22:11:16 +0300

    Add render-build.sh for system dependencies

--- render-build.sh
+++ render-build.sh
@@ -8,9 +8,14 @@ apt-get install -y \
     zlib1g-dev \
     libpng-dev \
     libfreetype6-dev \
-    libwebp-dev
+    libwebp-dev \
+    libtiff-dev
 
 # Install Python dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
